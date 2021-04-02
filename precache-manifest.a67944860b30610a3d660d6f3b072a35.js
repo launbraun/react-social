fb4aba1e53a38512091b0e248c831732c88a669b
launbraun <launbraun@bk.ru>
2021-04-02 16:23:19 +0300

    Updates

---
+++ precache-manifest.a67944860b30610a3d660d6f3b072a35.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d978df2b314f1a5b34b8cf3c5492f3a",
+    "url": "/react-social/index.html"
+  },
+  {
+    "revision": "c35b18132983c977e0fb",
+    "url": "/react-social/static/css/3.e685fcfb.chunk.css"
+  },
+  {
+    "revision": "a5be58e77a675d0097d8",
+    "url": "/react-social/static/css/4.d90377e7.chunk.css"
+  },
+  {
+    "revision": "68d1f046b080eb325f94",
+    "url": "/react-social/static/css/main.676532a4.chunk.css"
+  },
+  {
+    "revision": "4a84faf454d7b2aa5c17",
+    "url": "/react-social/static/js/2.2e389f06.chunk.js"
+  },
+  {
+    "revision": "c35b18132983c977e0fb",
+    "url": "/react-social/static/js/3.baac6047.chunk.js"
+  },
+  {
+    "revision": "a5be58e77a675d0097d8",
+    "url": "/react-social/static/js/4.0d0c8c23.chunk.js"
+  },
+  {
+    "revision": "68d1f046b080eb325f94",
+    "url": "/react-social/static/js/main.9899a1cd.chunk.js"
+  },
+  {
+    "revision": "46ef5c7d916c894c291d",
+    "url": "/react-social/static/js/runtime~main.88e2e5eb.js"
+  },
+  {
+    "revision": "6e7c9db7e1f100edac7c6e48714d2ea0",
+    "url": "/react-social/static/media/preloader.6e7c9db7.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/react-social/static/media/user.8b1dbb1f.png"
+  },
+  {
+    "revision": "cf5dd8dbf111e029a4abc0487801d490",
+    "url": "/react-social/static/media/vk_logo.cf5dd8db.png"
+  }
+]);
